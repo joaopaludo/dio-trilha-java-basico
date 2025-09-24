a43295ae6064888d76d73862c99b5b52208806e5
joaopaludo <joaopaludo@sysmo.com.br>
2025-09-23 22:15:17 -0300

    Implementa atividade

--- ContaBanco/src/ContaTerminal.java
+++ ContaBanco/src/ContaTerminal.java
@@ -1,5 +1,26 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+
+        System.out.println("Por favor, digite o número da conta:");
+        int conta = scanner.nextInt();
+        scanner.nextLine();
+
+        System.out.println("Por favor, informe a agência:");
+        String agencia = scanner.nextLine();
+
+        System.out.println("Por favor, informe o nome completo:");
+        String nome = scanner.nextLine();
+
+        System.out.println("Por favor, informe o saldo:");
+        double saldo = scanner.nextDouble();
+
+        System.out.printf(
+                "Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque.\n",
+                nome, agencia, conta, saldo);
     }
 }
